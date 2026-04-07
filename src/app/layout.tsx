f1cea2a5b4e10166dd8cb678ab4efa414c2ddef9
Ajil Pappachan <ajilpappachan@gmail.com>
2026-04-07 16:07:54 -0500

    Changed to dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}>
       <body className="min-h-full flex flex-col bg-background text-foreground">
         <Navbar />
         <main className="flex-1 max-w-7xl w-full mx-auto px-4 py-8">
